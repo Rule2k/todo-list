87ab46461dbc5596a511f2d683869a04075575c4
Rule2k <nicolaspaviot@gmail.com>
2021-03-11 19:08:46 +0100

    init

---
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HelloWorld.module.scss";
+
+const HelloWorld = () => {
+  return <div className={styles.HelloWorld}>Hello world !</div>;
+};
+
+export default HelloWorld;
